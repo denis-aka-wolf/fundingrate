372ddfc02b412c8d52ebbc1433f5167a0dc82d34
ACTPOHABT <denis.aka.wolf@gmail.com>
2025-12-26 15:56:20 +0300

    init

---
+++ bin/fundingrate.dart
@@ -0,0 +1,5 @@
+import 'package:fundingrate/fundingrate.dart' as fundingrate;
+
+void main(List<String> arguments) {
+  print('Hello world: ${fundingrate.calculate()}!');
+}
---
+++ test/fundingrate_test.dart
@@ -0,0 +1,8 @@
+import 'package:fundingrate/fundingrate.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
